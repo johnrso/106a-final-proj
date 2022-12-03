cf2db2c04b5111f278a52f99a3599adbabcf7406
John So <johnianrso@berkeley.edu>
2022-12-03 11:02:50 -0800

    test pub/sub model

--- src/planning/src/closed_loop_plan.py
+++ src/planning/src/closed_loop_plan.py
@@ -24,18 +24,18 @@ class PathPlannerNode(object):
     def __init__(self):
         rospy.init_node("path_plan", anonymous=True)
 
-        self.planner = PathPlanner("right_arm")
         self.buffer = []
-
-        self.dt = rospy.get_param("~dt")
         self.a_xy = 0
         self.a_z = -9.8
-        self.dt = 1/30 # camera frequency
+
+        self.planning_group = rospy.get_param("~planning_group", "right_arm")
+        self.dt = rospy.get_param("~dt", 1/30)
 
         pub_paths = rospy.get_param("~publishers")
         sub_paths = rospy.get_param("~subscribers")
 
-        self.odom_sub = rospy.Subscriber(sub_paths["odom"], PoseStamped, self.add_to_buffer)
+        self.planner = PathPlanner(self.planning_group)
+        self.pose_sub = rospy.Subscriber(sub_paths["ball_pose"], PoseStamped, self.add_to_buffer)
 
         thread = threading.Thread(target=self.plan, args=())
         thread.daemon = True  # Daemonize thread
@@ -49,28 +49,61 @@ class PathPlannerNode(object):
 
         # extract position from msg and append it to buffer
         self.buffer.append(np.array(msg.pose.position))
-
         # TODO: check which frame this is in
 
     def plan(self):
-        rospy.loginfo("path_planner: beginning planning server")
+        rospy.loginfo_once("path_planner: beginning planning server")
         while True:
             try:
                 buf_np = np.array(self.buffer)
-                n, _ = pos_samples.shape
+                n, _ = buf_np.shape
                 if buf_np.shape[0] > 1:
                     continue
 
                 # fit a parabola to the buffer
 
-                t = np.linspace(0, n*delta_t, n)
-                v_fit = fit_pos(t, pos_samples)
+                t = np.linspace(0, n*self.dt, n)
+                v_fit = fit_pos(t, buf_np)
 
                 # intercepts
                 x, y = xy_intercept(v_fit)
 
+                rospy.loginfo_once("path_planner: curve fit, beginning to plan")
+                current_pose = self.planner._group.get_current_pose()
 
+                goal = PoseStamped()
+                goal.header.frame_id = self.planner.ref_link
 
+                #x, y, and z position
+                goal.pose.position.x = x
+                goal.pose.position.y = y
+                goal.pose.position.z = self.current_pose.pose.position.z
+
+                #Orientation as a quaternion
+                goal.pose.orientation.x = 0.0
+                goal.pose.orientation.y = -1.0
+                goal.pose.orientation.z = 0.0
+                goal.pose.orientation.w = 0.0
+
+                pcm = PositionConstraint()
+                pcm.header.frame_id = self.planner.ref_link
+                pcm.link_name = self.planner.ee_link
+
+                cbox = SolidPrimitive()
+                cbox.type = SolidPrimitive.BOX
+                cbox.dimensions = [0.1, 0.4, 0.4] # TODO: possibly change
+                pcm.constraint_region.primitives.append(cbox)
+
+                cbox_pose = Pose()
+                cbox_pose.position.x = current_pose.pose.position.x
+                cbox_pose.position.y = 0.15
+                cbox_pose.position.z = 0.45
+                cbox_pose.orientation.w = 1.0
+                pcm.constraint_region.primitive_poses.append(cbox_pose)
+
+                plan = self.planner.plan_to_pose(goal, [], [pcm])
+                # if not self.planner.execute_plan(plan[1]):
+                #     raise Exception("Execution failed")
             except Exception as e:
                 print(e)
 if __name__ == "__main__":
